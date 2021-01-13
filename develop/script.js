a4267429f09bebd671ac265bba26354a4ffab7bf
lukenorys <lukenorys@ymail.com>
2021-01-12 20:51:40 -0600

    charset alerts/responses

--- develop/script.js
+++ develop/script.js
@@ -9,10 +9,19 @@ var lowerCase = confirm("How about lowercase letters?");
 var numbChara = confirm("Let's add some numbers in there, yes?");
 var specChara = confirm("How about some special characters to top it off?");
 
-// password length response
+// password length alert/response
 if (pwLength < 8 || pwLength > 128) {
   alert("Select your password length.")
   var pwLength = prompt("Please enter your criteria for a random password in the following prompts.")
+}
+
+// charset alerts/responses
+if (upperCase === false && lowerCase === false && numbChara === false && specChara === false){
+  alert("Pick at least one character set.");
+  var upperCase = confirm("Would you like to include uppercase letters?");
+  var lowerCase = confirm("How about lowercase letters?");
+  var numbChara = confirm("Let's add some numbers in there, yes?");
+  var specChara = confirm("How about some special characters to top it off?");
 }
 
 /* set arrays for above variables
