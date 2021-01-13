47e604ab6fb9289454e74e38227a99b0fb7cfae7
lukenorys <lukenorys@ymail.com>
2021-01-12 20:44:27 -0600

    start pw length fields

--- develop/script.js
+++ develop/script.js
@@ -3,16 +3,24 @@ var generateBtn = document.querySelector("#generate");	var generateBtn = documen
 
 // define variables
 var generate = confirm("Please enter your criteria for a random password in the following prompts.");
-var pwLenght = prompt("Type a number between 8 & 128 for password lenght. Example: 17");
+var pwLength = prompt("Type a number between 8 & 128 for password length. Example: 17");
 var upperCase = confirm("Would you like to include uppercase letters?");
 var lowerCase = confirm("How about lowercase letters?");
 var numbChara = confirm("Let's add some numbers in there, yes?");
 var specChara = confirm("How about some special characters to top it off?");
 
-// set arrays for above variables
+// password length response
+if (pwLength < 8 || pwLength > 128) {
+  alert("Select your password length.")
+  var pwLength = prompt("Please enter your criteria for a random password in the following prompts.")
+}
+
+/* set arrays for above variables
 var uCaseLet = {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  !!!try to add charset values instead of typing out each char. Working on this!!!
 }
+*/
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
